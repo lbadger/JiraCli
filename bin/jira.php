42bdeaef3d7db5ea8657a924a19f5b5f91a09b57
Wesley Curtis <wtcurtis@gmail.com>
2014-07-15 21:54:50 -0600

    All right, screw manually adding commands in. Glob the namespace and shove 'em in

--- bin/jira.php
+++ bin/jira.php
@@ -2,31 +2,20 @@
 <?php
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-use WCurtis\Command\FindCommand;
-use WCurtis\Command\StartTimerCommand;
-use WCurtis\Command\StopTimerCommand;
-use WCurtis\Command\LogCommand;
-use WCurtis\Command\LogListCommand;
-use WCurtis\Command\ConfigCommand;
-use WCurtis\Command\IssueCommand;
-use WCurtis\Command\ListTimersCommand;
-use WCurtis\Command\TimerKillCommand;
-use WCurtis\Command\LogTimerCommand;
-use WCurtis\Command\CommentListCommand;
-use WCurtis\Command\CommentAddCommand;
 use Symfony\Component\Console\Application;
 
 $app = new Application('jira-cli', '@package_version@');
-$app->add(new FindCommand());
-$app->add(new StartTimerCommand());
-$app->add(new StopTimerCommand());
-$app->add(new LogListCommand());
-$app->add(new LogCommand());
-$app->add(new ConfigCommand());
-$app->add(new IssueCommand());
-$app->add(new ListTimersCommand());
-$app->add(new TimerKillCommand());
-$app->add(new CommentListCommand());
-$app->add(new CommentAddCommand());
-$app->add(new LogTimerCommand());
+
+$commandNamespace = 'WCurtis\\Command\\';
+
+$files = glob(__DIR__ . '/../src/Command/*Command.php');
+
+foreach($files as $file) {
+    if(!preg_match('/\/([a-zA-Z]+Command)\.php$/', $file, $match)) continue;
+
+    $commandClass = $commandNamespace . $match[1];
+
+    $app->add(new $commandClass());
+}
+
 $app->run();
